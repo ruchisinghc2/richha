fe621a2791ada8c208ee29e930e1b1fe671a30f6
ruchisinghc2 <ruchisinghc2@gmail.com>
2026-09-13 00:15:02 +0530

    my chhitiya is very fool

---
+++ Ira.java
@@ -0,0 +1,16 @@
+	public class A{
+public static void main(String[]args){
+
+Scanner sc=new Scanner (System.in);
+
+System.out.pintln("enter the  number");
+
+int n=sc.nextInt(); if(n%2==0) { System.out.println("This is a even number");
+}
+else{
+System.out.println("this is odd number");}
+}
+}
+
+
+ 
